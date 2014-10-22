4dcbe791363c84facdb644e0c1088dcc2e85e692
pierfrancescoran <pfkjs@hotmail.it>
2014-10-22 20:38:39 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,40 +1,12 @@
 clear 
 
-
-sicstus_home=/usr/local/sicstus4.2.3
-dali_home=../DALI-master/src
-prolog="$sicstus_home/bin/sicstus"
-WAIT="ping -c 4 127.0.0.1" 
-main_home=mas
-
 #------------------------start MAS------------------------------------------------
 
-#clear history
+cd mas
 
-rm -rf mas/conf/mas/*
-
-#activate server DALI
-xterm -hold -e "$prolog -l $dali_home/active_server_wi.pl --goal \"go(3010,'server.txt').\"" & 
-echo Server activated. Activating MAS....
+xterm -hold -e "./startmas.sh" &
 $WAIT > /dev/null 
-
-xterm -hold -e "$prolog -l $dali_home/active_user_wi.pl --goal utente." & 
-echo Server DALI activated. Activating agents...
-$WAIT > /dev/null 
-
-#activating agents
-agent_base="robot1.txt"
-echo "Agente: $agent_base $prolog $dali_home $agent_filename"
-xterm -e "./conf/makeconf.sh $agent_base $dali_home" &
-xterm -hold -e "./conf/startagent.sh $agent_base $prolog $dali_home" &
-$WAIT > /dev/null # %WAIT% >nul
-
-agent_base="robot2.txt"
-echo "Agente: $agent_base $prolog $dali_home $agent_filename"
-xterm -e "./conf/makeconf.sh $agent_base $dali_home" &
-xterm -hold -e "./conf/startagent.sh $agent_base $prolog $dali_home" &
-$WAIT > /dev/null # %WAIT% >nul
-
+cd ..
 
 #---------------------------------start Gazebo--------------------------
 cd listener
